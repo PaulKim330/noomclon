6ab9a9bbc3108bca0ce8f261a630df69aeb3b64f
PaulKim330 <kms30423@gmail.com>
2023-04-12 13:57:23 +0900

    app.js & server.js 초기셋팅(nickname고정까지 완료)

---
+++ src/public/js/app.js
@@ -0,0 +1,68 @@
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName, nickName;
+
+function addMessage(message) {
+    const ul = room.querySelector("ul")
+    const li = document.createElement("li")
+    li.innerText =message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value,roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value ="";
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+}
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    span = room.querySelector("span");
+    span.innerText = `My nickname: ${nickName}`;
+    const msgForm = room.querySelector("#msg");
+
+    msgForm.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const inputRoomname = form.querySelector("#roomname");
+    const inputNickname = form.querySelector("#nickname");
+
+    roomName = inputRoomname.value;
+    nickName = inputNickname.value;
+    socket.emit("enter_room", roomName, nickName, showRoom);
+    inputRoomname.value = "";
+    inputNickname.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+    addMessage(`${user} arrived!`);
+});
+
+socket.on("bye", (left) => {
+    addMessage(`${left} left ㅠㅠ`);
+});
+
+socket.on("new_message", addMessage);
---
+++ src/server.js
@@ -0,0 +1,69 @@
+import http from "http";
+import { Server } from "socket.io";
+import path from 'path';
+const __dirname = path.resolve();
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+
+wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+    socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter);
+        console.log(`Socket Event:${event}`);
+    });
+
+    socket.on("enter_room", (roomName, nickname, done) => {
+        socket.join(roomName);
+        socket["nickname"] = nickname;
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname);
+    });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => 
+            socket.to(room).emit("bye", socket.nickname));
+    });
+
+    socket.on("new_message", (msg, roomName, done) => {
+        socket.to(roomName).emit("new_message",`${socket.nickname}: ${msg}`);
+        done();
+    });
+    
+    socket.on("nickname", nickname => (socket["nickname"] = nickname));
+});
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+    //     sockets.push(socket);
+    //     socket["nickname"] = "Anon";
+    //     console.log("Connected to Browser ✅");
+    //     socket.on("close", () => console.log("Disconnected from the Brower ❌"));
+    //     socket.on("message", (msg) => {
+        //         const message = JSON.parse(msg);
+//         switch(message.type){
+    //             case "new_message":
+    //                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+    //                 break
+    //             case "nickname":
+    //                 socket["nickname"] = message.payload;
+    //                 break
+    //         }
+    //     });
+    // });
+    
+    const handleListen = () => console.log(`Listening on http://localhost:3000`);
+    httpServer.listen(3000, handleListen);
+    
