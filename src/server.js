ecc7cb203b506b28abc9292d5f8768e8116b4be6
PaulKim330 <kms30423@gmail.com>
2023-04-12 18:00:22 +0900

    인원수 체크기능 완료

--- src/server.js
+++ src/server.js
@@ -16,8 +16,27 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+function publicRooms(){
+    const {
+        sockets: {
+            adapter: {sids, rooms},
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key)
+        }
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
 
 wsServer.on("connection", (socket) => {
+    wsServer.sockets.emit("room_change", publicRooms());
     socket["nickname"] = "Anon";
     socket.onAny((event) => {
         console.log(wsServer.sockets.adapter);
@@ -28,13 +47,18 @@ wsServer.on("connection", (socket) => {
         socket.join(roomName);
         socket["nickname"] = nickname;
         done();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms());
     });
-
+    
     socket.on("disconnecting", () => {
         socket.rooms.forEach(room => 
-            socket.to(room).emit("bye", socket.nickname));
-    });
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) -1));
+        });
+        
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    })
 
     socket.on("new_message", (msg, roomName, done) => {
         socket.to(roomName).emit("new_message",`${socket.nickname}: ${msg}`);
